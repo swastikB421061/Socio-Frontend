0b33697934f6e0d7c9a9cf2c093fe2a94e08842e
swastik pradhan <b421061@iiit-bh.ac.in>
2024-08-26 22:57:40 +0530

    after gppt

--- src/App.jsx
+++ src/App.jsx
@@ -14,69 +14,53 @@ import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-  const { data: authUser, isLoading } = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      try {
-        const res = await fetch("/api/auth/me");
-        const contentType = res.headers.get("Content-Type");
-        if (!contentType || !contentType.includes("application/json")) {
-          throw new Error("Expected JSON response but got something else");
-        }
-        const data = await res.json();
-        if (data.error) return null;
+	const { data: authUser, isLoading } = useQuery({
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			try {
+				const response = await fetch('/api/auth/me');
+				const contentType = response.headers.get('Content-Type');
+				console.log(contentType);
+				if (!contentType || !contentType.includes('application/json')) {
+				  throw new Error('Expected JSON response but got something else');
+				}
+			
+				const data = await response.json();
+			
+				if (!response.ok) {
+				  throw new Error(data.error || 'Something went wrong');
+				}
+			
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		retry: false,
+	});
 
-        if (!res.ok) {
-          throw new Error(data.error || "Something went wrong");
-        }
+	if (isLoading) {
+		return (
+			<div className='h-screen flex justify-center items-center'>
+				<LoadingSpinner size='lg' />
+			</div>
+		);
+	}
 
-        console.log("authUser is here:", data);
-        return data;
-      } catch (error) {
-        console.error("Fetch error:", error);
-        throw new Error(error.message || "Something went wrong");
-      }
-    },
-    retry: false,
-  });
-
-  if (isLoading) {
-    return (
-      <div className="h-screen flex justify-center items-center">
-        <LoadingSpinner size="lg" />
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex max-w-6xl mx-auto">
-      {authUser && <Sidebar />}
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/notifications"
-          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/profile/:username"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-      </Routes>
-      {authUser && <RightPanel />}
-      <Toaster />
-    </div>
-  );
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{authUser && <Sidebar />}
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+			</Routes>
+			{authUser && <RightPanel />}
+			<Toaster />
+		</div>
+	);
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
 		proxy: {
 			"/api": {
-				//target: "http://localhost:5000",
+				// target: "http://localhost:5000",
 				target: "https://socio-g7bt.onrender.com",
 				changeOrigin: true,
 			},
